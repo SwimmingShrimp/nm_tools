5258ba3183b1f175bec402dd085f860986f78c26
lixialin <875488840@qq.com>
2022-09-19 18:08:44 +0800

    20220919-2

--- img_modify/img_2_video.py
+++ img_modify/img_2_video.py
@@ -7,6 +7,10 @@ import argparse
 使用说明：需要配置图片所在文件夹的路径，另外还需要配置视频保存的文件夹路径/视频名称
 注意：需要修改压缩图片的分辨率
 '''
+def video_writer_function(output_name):
+    fourcc = cv2.VideoWriter.fourcc(*'MP4V')
+    video_writer = cv2.VideoWriter(filename=output_name, fourcc=fourcc, fps=7, frameSize=(844, 768))
+    return video_writer
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,24 +18,26 @@ def main():
     parser.add_argument('path_output',type=str,help='the path of video_output')
     args = parser.parse_args()
 
-    fourcc = cv2.VideoWriter.fourcc(*'MP4V')
-    video_writer = cv2.VideoWriter(filename=args.path_output, fourcc=fourcc, fps=2, frameSize=(844, 768))
-
     for (root,_,pics) in os.walk(args.folder_pic):   #pics为列表,而且列表内的元素是无序的
         if len(pics) != 0:
-           pics.sort(key=lambda x: int(x.rsplit('_')[-1].split('.')[0]))   #经过这步处理，此时pics已经是内部有序的列表
-           print(pics)
+            pics.sort(key=lambda x: int(x.rsplit('_')[-1].split('.')[0]))   #经过这步处理，此时pics已经是内部有序的列表
            #pics.sort(key=lambda x: int(x.split('.')[0]))
-           for pic in pics :
-               print(pic)
-               path_pic = os.path.join(root,pic)
-               img = cv2.imread(path_pic)
-               cv2.namedWindow('image',flags=cv2.WINDOW_NORMAL)
-               cv2.resizeWindow('image',(844,768))
-               cv2.imshow('image',img)
-               cv2.waitKey(100)
-               video_writer.write(img)
-               #print(str(i) + '.jpg' + ' done!')
+            print(pics)
+            j = 1
+            for i in range(len(pics)):
+                path_pic = os.path.join(root,pics[i])
+                img = cv2.imread(path_pic)
+                cv2.namedWindow('image',flags=cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('image',(844,768))
+                cv2.imshow('image',img)
+                cv2.waitKey(100)
+                if i == 0 or i%1500==0:
+                    dstfile = os.path.join(args.path_output,(str(j)+'.mp4'))
+                    j+=1
+                    video_write = video_writer_function(dstfile)
+                video_write.write(img)
+                
+
     # video_writer.release()
     # cv2.destroyAllWindows()
 
